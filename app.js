96d464992e76becb79cb66b45a9c2b460873471d
oscar-bek <salamov.behzod@gmail.com>
2022-09-23 11:36:30 +0900

    fix: mongoose connection va schema module qurib olish

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 
-// MongoDB connect
-const db = require("./server").db(); 
-const mongodb = require("mongodb");
 
 // 1 Kirish kodlari
 app.use(express.static("public"));
--- server.js
+++ server.js
@@ -2,20 +2,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const http = require("http");
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
 const connectionString = process.env.MONGO_URL;
 
-mongodb.connect(connectionString, {
+mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }, 
-  (err, client) => {
+  (err, goose) => {
    if (err) console.log("ERROR on connection MongoDB");
    else {
     console.log("MongoDB connection succed");
-
-    module.exports = client;
+   // console.log(goose);
     
     const app = require("./app");
     const server = http.createServer(app);
