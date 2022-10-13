8edafed85ae8bbdbfa63d10e026f659567ce9f5e
oscar-bek <salamov.behzod@gmail.com>
2022-10-13 18:19:12 +0900

    feat: task 9.1(chala) va 9.2 done

---
+++ test9-2.js
@@ -0,0 +1,24 @@
+// shunday bir function yasalsin, unga string pass qilinadi. 
+// stringda ham harf ham 1 ~ 9 gacha bolgan sonlar kiritilsin. 
+// symbollarni ishlatish mumkin emas,
+//  va sonlarni alohida harflarni alohida object qilib qaytaradi,
+
+
+
+function sortString (str) {
+    let textArray = str.split('')
+    let text = []
+    let num = []
+    textArray.forEach(e=>{
+        if (e>-1) {
+            num.push(e)
+        } else {
+            text.push(e)
+        }
+    })
+    return {text, num};
+}
+
+ const result = sortString("abs365fhd387");
+ console.log(result);
+
