493dbf863dc8b48b90ccacf6131471e7f56bd851
oscar-bek <salamov.behzod@gmail.com>
2022-10-21 19:47:19 +0900

    feat: task 13 and task 12

--- test12.js
+++ test12.js
@@ -3,16 +3,31 @@
 // => yakshanba bolgan yillarni bir arrayga joylab return qilsin;
 
 
-
-const findMeSundays = function (a, b) {
+const findMeSundays = function ( a, b )
+{
+  
   for (let year = a; year <= b; year++) {
     const d = new Date(year, 0, 1);
       if ( d.getDay() === 0 )
-          console.log( [ year ] );
+          console.log(year);
   }
 };
 
 findMeSundays(2014, 2050);
 
 
+function getSundays(y){
+  y=y|| new Date().getFullYear();
+  var A=[];
+  var D=new Date(y,0,1)
+  var day= D.getDay();
+  if(day!=0) D.setDate(D.getDate()+(7-day));
+  A[0]=D.toLocaleString();
+  while(D){
+      D.setDate(D.getDate()+7);
+      if(D.getFullYear()!=y) return A;
+      A.push(D.toLocaleDateString());
+  }
+}
 
+getSundays(2020);
--- test13.js
+++ test13.js
@@ -1,13 +1,13 @@
 const array = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-const s = 3;
+const s = 5;
 
     function codeMessage(array, s)
     {
         let result=""
         for (let i = 0; i < array.length; i++)
         {
-            let char = text[i];
-            if (char.toUpperCase(text[i]))
+            let char = array[i];
+            if (char.toUpperCase(array[i]))
             {
                 let ch =  String.fromCharCode((char.charCodeAt(0) + s-65) % 26 + 65);
                 result += ch;
@@ -21,4 +21,5 @@ const s = 3;
         return result;
     }
   
-codeMessage();
+console.log(codeMessage(array, s) );
+
