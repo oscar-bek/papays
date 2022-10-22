3477c1d4f02e2265b0ff597739c4d9701f0591b9
oscar-bek <salamov.behzod@gmail.com>
2022-10-22 20:58:48 +0900

    feat: task14 => converting two string to an array

---
+++ test14.js
@@ -0,0 +1,16 @@
+// ikkita stringni o'ziga argument qilib olib, har ikkalasida qatnashgan harflarni bir yangi arrayda return qilib qaytaradigan function yasang
+let a = "hello";
+let b = "everyone";
+
+
+findSimilarLetters = (a, b) => {
+let array = a + b;
+let arr = array.split();
+
+console.log(arr);
+}
+
+ findSimilarLetters(a, b);
+
+
+
