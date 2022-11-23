30cc3e91f17eb824ace216822d5175a69f83c064
oscar-bek <salamov.behzod@gmail.com>
2022-11-23 19:52:29 +0900

    feat: develop unsubscribe ape business logic

--- controllers/followController.js
+++ controllers/followController.js
@@ -16,3 +16,17 @@ followControlller.subscribe = async (req, res) => {
 		res.json({ state: 'fail', message: err.message });
   }
 };
+
+followControlller.unsubscribe = async (req, res) => {
+  try {
+    console.log('GET: cont/unsubscribe');
+    assert.ok(req.member, Definer.auth_err5);
+
+    const follow = new Follow();
+    await follow.unsubscribeData(req.member, req.body);
+    res.json({ state: "success", data: "unsubscribed" });
+  } catch (err) {
+    console.log(`ERROR, cont/unsubscribe ${err.message}`);
+		res.json({ state: 'fail', message: err.message });
+  }
+};
--- router.js
+++ router.js
@@ -100,4 +100,9 @@ router.post(
 	memberController.retrieveAuthMember,
 	followController.subscribe
 	);
+router.post(
+	"/follow/unsubscribe",
+	memberController.retrieveAuthMember,
+	followController.unsubscribe
+);
 module.exports = router;
