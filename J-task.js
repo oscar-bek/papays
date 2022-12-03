bbd44184e4dd16d1f9ef65923ef4bfc554cc7921
oscar-bek <salamov.behzod@gmail.com>
2022-12-03 17:48:55 +0900

    feat: J-task is done

---
+++ J-task.js
@@ -0,0 +1,7 @@
+//  J-TASK: shunday function yozingki u function ozini nomini qaytarsin. Masalan: function calculate return qilsin 'calculate'
+
+function Portugal() {
+  console.log(this.constructor.name); 
+}
+
+console.log(Portugal.prototype.constructor.name); 
