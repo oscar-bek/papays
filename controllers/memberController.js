17bb211376206de3c38dbcde6fb7b3a93a6ac20d
oscar-bek <salamov.behzod@gmail.com>
2022-12-18 01:26:08 +0900

    fiz: modify cookie credential configurations

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -15,7 +15,7 @@ memberController.signup = async (req, res) => {
 
               res.cookie("access_token", token, {
                   maxAge: 6 * 3600 * 1000,
-                  httpOnly: true,
+                  httpOnly: false,
               });
 
         res.json({ state: "success", data: new_member });
